f6186686c9a0a850942f6588b887a2919de21552
elf <elff1@outlook.com>
2025-10-20 15:06:02 +0800

    subcribe and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,20 @@
 use std::io;
 use std::net::TcpListener;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use actix_web::dev::Server;
 
-async fn health_check(_req: HttpRequest) -> impl Responder {
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn health_check(_req: HttpRequest) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    //req.
     HttpResponse::Ok().finish()
 }
 
@@ -11,6 +22,7 @@ pub fn run(tcp_listener: TcpListener) -> io::Result<Server> {
     let server = HttpServer::new(|| {
             App::new()
                 .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
         })
         .listen(tcp_listener)?
         .run();
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -17,6 +17,47 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let address = spawn_app();
+    let client = Client::new();
+
+    let form_data = "name=le%20guin&email=le_guin%40gmail.com";
+    let response = client
+        .post(format!("{address}/subscriptions"))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(form_data)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_form_data() {
+    let address = spawn_app();
+    let client = Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing email"),
+        ("email=le_guin%40gmail.com", "missing name"),
+        ("", "missiing name and email"),
+    ];
+
+    for (invalid_data, error_msg) in test_cases {
+        let response = client
+            .post(format!("{address}/subscriptions"))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_data)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(400, response.status().as_u16(), "failed of case {}", error_msg);
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
